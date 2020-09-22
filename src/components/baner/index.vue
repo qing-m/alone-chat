fdf153922d4cee7ba13d4fe13fbed0738ab53e5f
wangheyao <wangheyao@zealfi.com>
2020-09-22 10:41:49 +0800

    fix

---
+++ src/components/baner/index.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="baner-wrapper">
+    <div class="bruce">
+      <div class="parallax"></div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Baner"
+};
+</script>
+
+<style lang="scss">
+$bg: "https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1600752451394&di=2b55642bcae2446fc06a3c547815026f&imgtype=0&src=http%3A%2F%2Fp2-q.mafengwo.net%2Fs11%2FM00%2FB6%2F7B%2FwKgBEFt_tvGAFvHxAAFIRKQnOBw21.jpeg";
+.baner-wrapper {
+  .bruce {
+    .parallax {
+      width: 100%;
+      height: 100vh;
+      background-image: url($bg);
+      background-repeat: no-repeat;
+      background-position: center;
+      background-attachment: fixed;
+      background-size: 100% 100%;
+    }
+  }
+}
+.baner-wrapper::-webkit-scrollbar {
+  display: none;
+}
+</style>
--- src/views/Home/index.vue
+++ src/views/Home/index.vue
@@ -1,34 +1,17 @@
 <template>
   <div class="home-wrapper">
-    <div class="bruce">
-      <div class="parallax-scrolling"></div>
-      <div class="content">111111111111</div>
-    </div>
+    <baner />
   </div>
 </template>
 
 <script>
+import baner from "@/components/baner";
 export default {
-  name: "Home"
+  name: "Home",
+  components: {
+    baner
+  }
 };
 </script>
 
-<style lang="scss">
-$bg: "https://i.loli.net/2019/12/25/Fze9jchtnyJXMHN.jpg";
-.home-wrapper {
-  .bruce {
-    .parallax-scrolling {
-      width: 100%;
-      height: 100vh;
-      background: url($bg) no-repeat center fixed;
-      background-size: 100% 100%;
-    }
-    .content {
-      height: 2000px;
-    }
-  }
-}
-.home-wrapper::-webkit-scrollbar {
-  display: none;
-}
-</style>
+<style lang="scss"></style>
